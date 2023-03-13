c1f19a5f0cee453fccb483f654f02f7265292347
Cody Lobdell <thecreator85@gmail.com>
2023-03-13 15:19:44 -0700

    finsihed backend for the day

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ mongoose.connect(process.env.DB_URL);
 const app = express();
 app.use(cors());
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 app.get('/book', getBooks);
 
